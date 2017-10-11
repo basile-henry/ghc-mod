ead77e0ef33c297aededd515ad989eeba08f9b94
Basile Henry <bjm.henry@gmail.com>
2017-10-11 11:14:59 +0200

    Fix dependencies

--- Setup.hs
+++ Setup.hs
@@ -1,18 +1,9 @@
 #!/usr/bin/env runhaskell
 
-{-# LANGUAGE CPP #-}
+import           Distribution.Simple
+import           Distribution.Simple.Program
 
-#ifndef MIN_VERSION_cabal_doctest
-#error \
-    Your version of cabal-install does not seem to support the 'custom-setup' section. \
-    Please see https://github.com/DanielG/ghc-mod/wiki/Installing#checking-and-installing-prerequisites for instrutions on how to upgrade. \
-    It is also possible that you forgot to install cabal-doctest before running Setup.hs
-#endif
-
-import Distribution.Simple
-import Distribution.Simple.Program
-
-import Distribution.Extra.Doctest
+import           Distribution.Extra.Doctest
 
 main :: IO ()
 main =
